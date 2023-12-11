24b079afea130d9157d8040f39b286718b47148e
Herbert Albuquerque <herbertrj87@icloud.com>
2023-12-11 20:54:43 -0300

    template initial

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def home(request):  
-    return render(request, 'recipes/home.html')
-
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
